cc5c5d3e73e8281f130c27e57212f09a7fc87cc1
jyw2 <jyw2@ualberta.ca>
2021-09-02 17:04:18 -0600

    component templates genertated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { GrindSpotComponent } from './grind-spot/grind-spot.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { AddSessionComponent } from './add-session/add-session.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserGrindSpotComponent } from './user-grind-spot/user-grind-spot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RankingComponent,
+    GrindSpotComponent,
+    GraphsComponent,
+    AddSessionComponent,
+    LoginComponent,
+    SignUpComponent,
+    UserGrindSpotComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/grind-spot/grind-spot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grind-spot',
+  templateUrl: './grind-spot.component.html',
+  styleUrls: ['./grind-spot.component.css']
+})
+export class GrindSpotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
