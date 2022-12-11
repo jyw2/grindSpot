99ec9362d7d8e45fd8acd7e99d12e03234d17d64
josh <jyw2@ualberta.ca>
2022-12-10 19:21:32 -0700

    Change api endpoint

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,34 +12,34 @@ export class FormComponent implements OnInit, OnDestroy {
   //Component for a user to add a grind session
 
   //default form values
-  public before:string
-  public after:string
-  public gain:string
-  public time:string
-  public class:string = ''
-  @Input()public spot:string = ''//accesed on grindspot pages
-  @Input() public defaultSpot:string
-  public AP:string
-  public DP:string
-  public agris:boolean = false
-  public boosts:string
-  public private:boolean = false
+  public before: string
+  public after: string
+  public gain: string
+  public time: string
+  public class: string = ''
+  @Input() public spot: string = ''//accesed on grindspot pages
+  @Input() public defaultSpot: string
+  public AP: string
+  public DP: string
+  public agris: boolean = false
+  public boosts: string
+  public private: boolean = false
 
   //alerts
-  public invalid:boolean = false
-  public invalidMessage:string
-  public successMessage:string
-  public success:boolean = false
+  public invalid: boolean = false
+  public invalidMessage: string
+  public successMessage: string
+  public success: boolean = false
 
   //login vars
-  public loggedIn:boolean
+  public loggedIn: boolean
   private loggedInSub: Subscription
 
   //options from the game
-  public classes:string[]
-  public spots:string[]
+  public classes: string[]
+  public spots: string[]
 
-  constructor(private data:CentralData, private http:HttpClient ) { }
+  constructor(private data: CentralData, private http: HttpClient) { }
 
   ngOnInit(): void {
     //get centralized data
@@ -48,7 +48,7 @@ export class FormComponent implements OnInit, OnDestroy {
     this.loggedIn = this.data.getLoginState()
 
     //update login state when logged in
-    this.loggedInSub =this.data.getChangesSubj().subscribe((state) =>{
+    this.loggedInSub = this.data.getChangesSubj().subscribe((state) => {
       this.loggedIn = state
     })
 
@@ -59,20 +59,20 @@ export class FormComponent implements OnInit, OnDestroy {
     this.loggedInSub.unsubscribe()
   }
 
-  packageData(){
+  packageData() {
     //Package object to send with the post request for the grind session
     let sph
-    if(this.gain){
-      sph = (parseInt(this.gain)/(parseInt(this.time)/60))
-    }else{
-      sph = (parseInt(this.after)-parseInt(this.before))/(parseInt(this.time)/60)
+    if (this.gain) {
+      sph = (parseInt(this.gain) / (parseInt(this.time) / 60))
+    } else {
+      sph = (parseInt(this.after) - parseInt(this.before)) / (parseInt(this.time) / 60)
     }
     sph = Math.floor(sph)
 
     let dt = new Date()
-    let dtString = dt.getFullYear() +'/'+
-    (dt.getMonth() < 10? '0'+(dt.getMonth()+1): dt.getMonth() )
-    +'/'+ (dt.getDate() < 10? '0'+dt.getDate(): dt.getDate() )
+    let dtString = dt.getFullYear() + '/' +
+      (dt.getMonth() < 10 ? '0' + (dt.getMonth() + 1) : dt.getMonth())
+      + '/' + (dt.getDate() < 10 ? '0' + dt.getDate() : dt.getDate())
 
 
 
@@ -84,78 +84,77 @@ export class FormComponent implements OnInit, OnDestroy {
       agris: this.agris,
       boosts: this.boosts ? this.boosts : 0,
       date: dtString,
-      id:this.data.getLoginState()? localStorage.getItem('token'):'',
+      id: this.data.getLoginState() ? localStorage.getItem('token') : '',
       private: this.private
     }
     return pack
   }
 
-  formSubmit(){
+  formSubmit() {
     //sends form to data base
-      this.hideAlerts()
+    this.hideAlerts()
 
     //validate for no empty or negative inputs
-      if(this.AP && this.DP && (this.gain || (this.before && this.after)) &&
-      this.time &&this.spot && this.class){
-        if(+this.AP > 0 && +this.DP > 0 && (+this.gain > 0 || (+this.before > 0 && +this.after > 0)) && +this.time > 0)
-        {
-           //validation ok
+    if (this.AP && this.DP && (this.gain || (this.before && this.after)) &&
+      this.time && this.spot && this.class) {
+      if (+this.AP > 0 && +this.DP > 0 && (+this.gain > 0 || (+this.before > 0 && +this.after > 0)) && +this.time > 0) {
+        //validation ok
 
-            this.http.post('https://api.jyuenw.com/grind/spot/' + this.spot, this.packageData()).subscribe((res:any)=>{
-            // this.http.post('http://localhost:3001/grind/spot/'+ this.spot, this.packageData()).subscribe((res:any)=>{
-              if (res.success){
-                //session added
-                this.clear()
-                this.successAlert('Grind session succesfully added!')
-              }else{
-                //session add failed on server side logic
-                this.invalidAlert('Grind session could not be added, our servers are currently having problems')
-              }
-            },()=>{
-              //call failed
-              this.invalidAlert('Grind session could not be added, please try again.')
-            })
+        this.http.post('https://jyuenwapi.azurewebsites.net/grind/spot/' + this.spot, this.packageData()).subscribe((res: any) => {
+          // this.http.post('http://localhost:3001/grind/spot/'+ this.spot, this.packageData()).subscribe((res:any)=>{
+          if (res.success) {
+            //session added
+            this.clear()
+            this.successAlert('Grind session succesfully added!')
+          } else {
+            //session add failed on server side logic
+            this.invalidAlert('Grind session could not be added, our servers are currently having problems')
+          }
+        }, () => {
+          //call failed
+          this.invalidAlert('Grind session could not be added, please try again.')
+        })
 
-        }else{
-          //negative numbers
-          this.invalidAlert('Grind session could not be added, negative values are not allowed')
-        }
-      }else{
-        //non valid inputs
-        this.invalidAlert('Grind session could not be added, please check that all required inputs were entered and try again.')
+      } else {
+        //negative numbers
+        this.invalidAlert('Grind session could not be added, negative values are not allowed')
       }
+    } else {
+      //non valid inputs
+      this.invalidAlert('Grind session could not be added, please check that all required inputs were entered and try again.')
+    }
 
 
 
   }
 
-  hideAlerts(){
-     this.invalid= false
-     this.success= false
+  hideAlerts() {
+    this.invalid = false
+    this.success = false
   }
 
-  successAlert(message:string){
+  successAlert(message: string) {
     this.success = true
     this.successMessage = message
   }
 
-  invalidAlert(message:string){
+  invalidAlert(message: string) {
     this.invalid = true
     this.invalidMessage = message
   }
 
-  swapGain(){
+  swapGain() {
     //use delta silver for calculations
-    this.after =''
+    this.after = ''
     this.before = ''
   }
 
-  swapBF(){
+  swapBF() {
     //use net gain for s/h calculations
     this.gain = ''
   }
 
-  clear(){
+  clear() {
     //clear form
     (this.before as string) = ''
     this.after = ''
